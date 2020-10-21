b3bc2cb5cc538d0c76f916384149ab40b807137b
= <=>
2020-10-21 13:05:12 +0700

    add mediaquery

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,83 +18,168 @@ class _HomeScreenState extends State<HomeScreen> {
           title: Text('Food And Drink Menu', style: TextStyle(fontFamily: 'DancingScript', fontWeight: FontWeight.bold)),
           backgroundColor: Colors.teal
       ),
-      body: ListView(
-        children: [
-          Container(
-            child: GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(context, FoodScreen.id);
-              },
-              child: Container(
-                height: 200,
-                margin: EdgeInsets.all(7),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/chicken.png"),
-                        fit: BoxFit.cover
-                    ),
-                    borderRadius: BorderRadius.circular(7),color: Colors.black12
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+        ? ListView(children: portraitContent())
+        : ListView(scrollDirection: Axis.horizontal, children: landscapeContent())
+    );
+  }
+
+  List<Widget> landscapeContent(){
+    return <Widget>[
+      Container(
+        width:250,
+        margin: EdgeInsets.fromLTRB(10, 10, 5, 100),
+        child: Container(
+          child: GestureDetector(
+            onTap: (){
+              Navigator.pushNamed(context, FoodScreen.id);
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/chicken.png")
                 ),
+                borderRadius: BorderRadius.circular(7),color: Colors.black12
               ),
             ),
-          ),
-          Container(
+          )
+        )
+      ),
+      Container(
+          width:250,
+          margin: EdgeInsets.fromLTRB(5, 10, 5, 100),
+          child: Container(
             child: GestureDetector(
               onTap: (){
                 Navigator.pushNamed(context, DrinkScreen.id);
               },
               child: Container(
-                height: 200,
-                margin: EdgeInsets.all(7),
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/drinks.png"),
-                        fit: BoxFit.cover
-                    ),
-                    borderRadius: BorderRadius.circular(7),color: Colors.black12
+                  image: DecorationImage(
+                    image: AssetImage("images/drinks.png")
+                  ),
+                  borderRadius: BorderRadius.circular(7),color: Colors.black12
                 ),
               ),
-            ),
-          ),
-          Container(
+            )
+          )
+      ),
+      Container(
+          width:250,
+          margin: EdgeInsets.fromLTRB(5, 10, 5, 100),
+          child: Container(
             child: GestureDetector(
               onTap: (){
                 Navigator.pushNamed(context, SaladScreen.id);
               },
               child: Container(
-                height: 200,
-                margin: EdgeInsets.all(7),
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/salad.png"),
-                        fit: BoxFit.cover
-                    ),
-                    borderRadius: BorderRadius.circular(7),color: Colors.black12
+                  image: DecorationImage(
+                    image: AssetImage("images/salad.png")
+                  ),
+                  borderRadius: BorderRadius.circular(7),color: Colors.black12
                 ),
               ),
-            ),
-          ),
-          Container(
+            )
+          )
+      ),
+      Container(
+          width:250,
+          margin: EdgeInsets.fromLTRB(5, 10, 10, 100),
+          child: Container(
             child: GestureDetector(
               onTap: (){
                 Navigator.pushNamed(context, SidesScreen.id);
               },
               child: Container(
-                height: 200,
-                margin: EdgeInsets.all(7),
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/sides.png"),
-                        fit: BoxFit.cover
-                    ),
-                    borderRadius: BorderRadius.circular(7),color: Colors.black12
+                  image: DecorationImage(
+                    image: AssetImage("images/sides.png")
+                  ),
+                  borderRadius: BorderRadius.circular(7),color: Colors.black12
                 ),
               ),
-            ),
+            )
           )
-        ],
+      )
+    ];
+  }
+
+  List<Widget> portraitContent(){
+    return <Widget>[
+      Container(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.pushNamed(context, FoodScreen.id);
+          },
+          child: Container(
+            height: 200,
+            margin: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/chicken.png"),
+                    fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.circular(7),color: Colors.black12
+            ),
+          ),
+        ),
       ),
-    );
+      Container(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.pushNamed(context, DrinkScreen.id);
+          },
+          child: Container(
+            height: 200,
+            margin: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/drinks.png"),
+                    fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.circular(7),color: Colors.black12
+            ),
+          ),
+        ),
+      ),
+      Container(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.pushNamed(context, SaladScreen.id);
+          },
+          child: Container(
+            height: 200,
+            margin: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/salad.png"),
+                    fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.circular(7),color: Colors.black12
+            ),
+          ),
+        ),
+      ),
+      Container(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.pushNamed(context, SidesScreen.id);
+          },
+          child: Container(
+            height: 200,
+            margin: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/sides.png"),
+                    fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.circular(7),color: Colors.black12
+            ),
+          ),
+        ),
+      )
+    ];
   }
 }
 
